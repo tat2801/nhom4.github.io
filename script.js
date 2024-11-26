b0a9fec73e2a98e5701bba707730005307e17b46
TAT <thutran2264826@gmail.com>
2024-11-26 19:55:14 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  alert(`Thank you, ${name}! We will contact you at ${email}.`);
+});
