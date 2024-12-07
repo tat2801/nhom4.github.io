b01f99215fb6fb17e087e6cb8606e4060fe0f664
TAT <thutran2264826@gmail.com>
2024-12-07 14:49:17 +0700

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,7 +1,9 @@
+
 document.querySelector('.product-gallery').addEventListener('wheel', (e) => {
   e.preventDefault();
   e.currentTarget.scrollBy({
-    left: e.deltaY > 0 ? 100 : -100,
+    left: e.deltaY > 0 ? 100 : -100, // Scroll right for down, left for up
     behavior: 'smooth',
   });
 });
+
